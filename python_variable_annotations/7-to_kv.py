96277d0b9ec5c670f6534ee3d73f491b4a8fd91d
Igboerika Onochie Onyedikachi <o.igboerika@alustudent.com>
2025-01-09 18:17:23 +0200

    kyiv

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -4,5 +4,5 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-   """Returns tuple of string and square of number"""
-   return (k, float(v ** 2))
+    """Returns tuple of string and square of number"""
+    return (k, float(v ** 2))
