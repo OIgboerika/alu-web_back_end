833529996849fba1b8ee219d5c93a2d089e74ead
Igboerika Onochie Onyedikachi <o.igboerika@alustudent.com>
2025-01-09 14:39:41 +0200

    so in summary...

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""adding annotations to sum_list function"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """adding annotations to sum_list function"""
+    return sum(input_list)
