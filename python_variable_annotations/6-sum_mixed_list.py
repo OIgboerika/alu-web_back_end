ca72dcf7c86866401cc812f7cf2a2e77d1b3a7ff
Igboerika Onochie Onyedikachi <o.igboerika@alustudent.com>
2025-01-09 15:06:30 +0200

    float the union intruder

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -4,5 +4,5 @@ from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-   """Returns sum of mixed list of ints and floats as a float"""
-   return float(sum(mxd_lst))
+    """Returns sum of mixed list of ints and floats as a float"""
+    return float(sum(mxd_lst))
