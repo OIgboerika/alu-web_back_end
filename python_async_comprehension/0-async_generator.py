244c63f876fc8ad95cfaa4b35c61a1e8a8da3e38
Igboerika Onochie Onyedikachi <o.igboerika@alustudent.com>
2025-01-09 19:03:24 +0200

    rando numbo

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -11,4 +11,5 @@ async def async_generator() -> Generator[float, None, None]:  # type: ignore
     """generate random list of numbers"""
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)    
+        yield random.uniform(0, 10)
+  
