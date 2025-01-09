8a9f1710a391ffb4e81d88236a797e22f468aad6
Igboerika Onochie Onyedikachi <o.igboerika@alustudent.com>
2025-01-09 15:05:15 +0200

    float the union intruder

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Contains typed function sum_mixed_list"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+   """Returns sum of mixed list of ints and floats as a float"""
+   return float(sum(mxd_lst))
